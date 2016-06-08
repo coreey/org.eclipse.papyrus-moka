06ab6e197c0e5205584fa9264681d464f8bef903
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2016-06-08 14:58:04 +0200

    Update promote script to make sure right Eclipse-SDK is used
    Change-Id: I95acb62363a22c94a10c2b81b4ac64bbae5cab86
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

--- releng/org.eclipse.papyrus.moka.p2/publish.sh
+++ releng/org.eclipse.papyrus.moka.p2/publish.sh
@@ -207,9 +207,9 @@ cd $tmpDir
 # Download and prepare Eclipse SDK, which is needed to merge update site and postprocess repository 
 echo "Downloading eclipse to $PWD"
 
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.4.2-201502041700/eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz .
-tar -xzf eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz
-cd eclipse
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.5.2-201602121500/eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gz .
+ls -la
+tar -xzf eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gzcd eclipse
 chmod 700 eclipse
 cd ..
 if [ ! -d "eclipse" ];
@@ -222,7 +222,7 @@ echo "Installing WTP Releng tools"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/webtools/releng/repository/ -installIUs org.eclipse.wtp.releng.tools.feature.feature.group
 # Clean up
 echo "Cleaning up"
-rm eclipse-SDK-4.4.2-linux-gtk-x86_64.tar.gz
+rm eclipse-SDK-4.5.2-linux-gtk-x86_64.tar.gz
 
 # Generate drop files
 if [ "$dropFiles" = y ];
